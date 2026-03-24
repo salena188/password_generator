24de3a9d7aeef5000b28de28573d01ffcc05bd19
salena188 <salinashrestha188@gmail.com>
2026-03-24 21:35:55 +0545

    firstcommit

---
+++ password-generator-save/__init__.py
@@ -0,0 +1,2 @@
+from .password_generator import PasswordGenerator
+
---
+++ password-generator-save/password_generator.py
@@ -0,0 +1,10 @@
+class PasswordGenerator:
+    def generate(self):
+        print("🔐 Password generated successfully")
+
+
+# ⭐ This makes the file runnable
+if __name__ == "__main__":
+    my_generator = PasswordGenerator()
+    my_generator.generate()
+    
