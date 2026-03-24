e6a6dc3760b2652f51dc904aacbe70de067eaa59
salena188 <salinashrestha188@gmail.com>
2026-03-24 21:38:35 +0545

    learning

---
+++ README.md
@@ -0,0 +1,10 @@
+# Password Generator & Saver
+
+A simple Python project that generates strong passwords and saves them to a file.
+
+## Features
+
+- Generate random strong passwords
+- Save credentials to file
+- Beginner-friendly project
+- 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="password_generator",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Salina Shrestha",
+    description="Simple Password Generator Project",
+)
